c21f38bd643815696f8b5ef51c20627bf28a1ef3
Henrique <henrique.plima1@gmail.com>
2019-05-05 13:25:35 -0300

    implementando ligacao

--- app/src/main/java/com/hp/project/finalprojectandroid/featureSobre/AboutAppFragment.kt
+++ app/src/main/java/com/hp/project/finalprojectandroid/featureSobre/AboutAppFragment.kt
@@ -1,5 +1,7 @@
 package com.hp.project.finalprojectandroid.featureSobre
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -7,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.hp.project.finalprojectandroid.R
 import kotlinx.android.synthetic.main.fragment_about_app.*
+import java.net.URI
 
 
 class AboutAppFragment : Fragment() {
@@ -36,6 +39,12 @@ class AboutAppFragment : Fragment() {
             val pInfo = context.packageManager.getPackageInfo(context.packageName,0)
             val version = pInfo.versionName
             tvVersion.setText("Versao: "+version)
+        }
+
+        btLigar.setOnClickListener {
+            val tel = "01155879654"
+            val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$tel"))
+            startActivity(intent)
         }
 
     }
